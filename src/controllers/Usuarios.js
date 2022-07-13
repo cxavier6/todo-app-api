4dfa121b3aef96ee1597822e81dce9d0df463b02
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-07-13 19:44:29 -0300

    feat: criado model da entidade usuarios

--- src/controllers/Usuarios.js
+++ src/controllers/Usuarios.js
@@ -1,7 +1,11 @@
+import UsuarioModel from "../models/UsuarioModel.js"
+
 class Usuarios{
     static rotas(app){
-        app.get('/usuarios', (req, res) => 
-        res.send("rota usuários"))
+        app.get('/usuarios', (req, res) => {
+        const usuario = new UsuarioModel("José", "josé@couve.com","219999999")
+        res.send(usuario)
+        })
     }
 }
 
